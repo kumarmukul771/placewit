31a3263bb9995cd6622abd922cd659c022f89215
Mukul Kumar <kumarmukul771@gmail.com>
2022-03-19 14:47:36 +0530

    Color game completed easy with comments

--- JS/ColorGame/app.js
+++ JS/ColorGame/app.js
@@ -1,16 +1,24 @@
 console.log("Included");
 const newColor = () => {
+  // This will return random values between 0 to 255
+  // First method
   let a = Math.floor(Math.random() * 256);
+  // Second method
   let b = Math.floor(Math.random() * 1000) % 256;
   let c = Math.floor(Math.random() * 1000) % 256;
   return `rgb(${a},${b},${c})`;
 };
 
 const check = (i, boxID, answerColor) => {
+  // If box selected is the wrong box
   if (i != boxID) {
+    // First method
     // document.getElementsByClassName("square")[i].style.opacity = "0";
+    // Second method
     document.getElementsByClassName("square")[i].style.visibility = "hidden";
-  } else {
+  }
+  // If box selected is the correct box
+  else {
     document.getElementsByClassName("top")[0].style.backgroundColor =
       answerColor;
     document.getElementById("correct").style.visibility = "visible";
@@ -22,17 +30,17 @@ const check = (i, boxID, answerColor) => {
     }
   }
 
+  //   Irrespective of the answer we want to give option to play-again
   document.getElementById("play-again").innerHTML = "Play again";
 };
 
 function initialize() {
   let answerColor = newColor();
+
+  // Another method to give color to each box
   //   let boxColor1 = newColor();
   //   let boxColor2 = newColor();
   //   let boxColor3 = newColor();
-
-  let boxID = `${Math.floor(Math.random() * 3)}`;
-
   //   document.getElementsByClassName("square")[0].style.backgroundColor =
   //     boxColor1;
   //   document.getElementsByClassName("square")[1].style.backgroundColor =
@@ -40,14 +48,20 @@ function initialize() {
   //   document.getElementsByClassName("square")[2].style.backgroundColor =
   //     boxColor3;
 
+  // ID of correct box
+  let boxID = `${Math.floor(Math.random() * 3)}`;
+
   for (let i = 0; i < 3; i++) {
     document.getElementsByClassName("square")[i].style.backgroundColor =
       newColor();
+    //   Want to pass arguments to function , so bind will help to create new function
+    //  whose first arg is null that is we have not defined what this in check refers to
+    // because it is not significant in this case.
     document.getElementsByClassName("square")[i].onclick = check.bind(
       null,
-      i,
-      boxID,
-      answerColor
+      i, // Chosen box
+      boxID, // Id of correct box
+      answerColor // Color of answer box
     );
     document.getElementsByClassName("square")[i].style.visibility = "visible";
   }
@@ -62,6 +76,7 @@ function initialize() {
 initialize();
 document.getElementById("play-again").onclick = initialize;
 
+// Bind function
 // let obj = {
 //   firstName: "Mukul",
 //   lastName: "Kumar",
